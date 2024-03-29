3dc4cb3281cf64ee9f11f752893101c7c48bc090
Rmoney <DionisisLiontos@outlook.com>
2024-03-29 20:57:57 +0200

    Routing and main menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "event-bind-example", component: EventBindExampleComponent},
+    {path: "welcome", component: WelcomeComponent},
+    {path: "", redirectTo: "/welcome", pathMatch: "full"},
+    {path: "person-table-component", component: PersonTableComponent}
+];
